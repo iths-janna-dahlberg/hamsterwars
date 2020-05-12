671303ca0f00f3588e05d08b3eb07cdef52bc8de
iths-janna-dahlberg <janna.dahlberg@gmail.com>
2020-05-12 23:46:32 +0200

    importin json to firebase etc

---
+++ public/script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyBQTfEjj2Iw9N5lqTy3qWqosQA0VicvSiM",
+  authDomain: "hamsterwars-d1705.firebaseapp.com",
+  databaseURL: "https://hamsterwars-d1705.firebaseio.com",
+  projectId: "hamsterwars-d1705",
+  storageBucket: "hamsterwars-d1705.appspot.com",
+  messagingSenderId: "105253758337",
+  appId: "1:105253758337:web:dc9108de51aa0925c2b480",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,3 +1,8 @@
 const { Router } = require("express");
 const router = new Router();
-const { data, update } = require("./../data")
+const { data, update } = require("./../db")
+
+router.get('/:id/:name', async (req, res) => {
+    let id = req.params.id;
+    let name = (req.params.name);
+})
