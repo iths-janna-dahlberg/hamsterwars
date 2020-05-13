6d7f97c2bde8e03c3c8cb07cef1c11d3a1205b1b
iths-janna-dahlberg <janna.dahlberg@gmail.com>
2020-05-13 23:38:22 +0200

    commit again

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 const hamstersRoute = require("./routes/hamsters");
 app.use("/hamsters", hamstersRoute);
 
+
 app.listen(3000, () => {
   console.log("Server is up and running!");
 });
--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,16 +1,63 @@
+const { db } = require("./../firebase");
 const { Router } = require("express");
-const { auth, db } = require("./../firebase");
 
 const router = new Router();
 
-router.get("/:id", async (req, res) => {
-  let docs = [];
-  let snapShot = await db.collection("hamsters").doc(req.params.id).get();
-  snapShot.forEach((doc) => {
-    docs.push(doc.data());
+//GET all Hamsters
+
+router.get("/", async (req, res) => {
+
+  let hamsters = [];
+
+  let snapShot = await db.collection("hamsters").get();
+
+  snapShot.forEach(doc => {
+    hamsters.push(doc.data());
   });
 
-  res.send({ hamsters: docs });
+  res.send({ hamsters: hamsters });
 });
 
+
+// GET hamster by id
+
+router.get("/:id", async (req, res) => {
+
+  let id = req.params.id;
+
+  let snapShot = await db.collection("hamsters").doc(req.params.id).get();
+  snapShot.forEach(doc => {
+    hamsters.push(doc.data());
+  });
+
+  res.send({ hamster: id })
+
+});
+
+// Update hamster with ID :id with new results
+
+router.put("/:id/results", async (req, res) => {
+
+  try {
+
+    let snapShot = await db.collection("hamsters").where("id", "==", req.params.id).get();
+    snapShot.forEach(doc => {
+
+      let hamster = doc.data();
+
+      hamster.wins += (req.body.wins * 1);
+      hamster.defeats += (req.body.wins * 1);
+      hamster.games += (req.body.wins * 1);
+
+      db.collection("hamsters").doc(doc.id).set(hamster)
+        .then(res.send({ msg: "Hamster result updated." }))
+        .catch(err => console.error(err))
+    })
+  }
+
+  catch (err) {
+    console.error(err);
+
+  }
+})
 module.exports = router;
